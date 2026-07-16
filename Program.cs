b93705ed967bdc4f3e850737bd0b25ae4649d856
DamianCG00 <marco.coba110107@gmail.com>
2026-07-15 22:41:48 -0600

    feat: implementacion completa de sistema de autenticacion Identity, vistas de Login y configuracion de seguridad

--- Program.cs
+++ Program.cs
@@ -3,32 +3,29 @@ using CitaApp.Web.Interfaces;
 using CitaApp.Web.Repositories;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using CitaApp.Web.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 // Inyección de dependencias actual
 builder.Services.AddScoped<IPacienteRepository, JsonPacienteRepository>();
 builder.Services.AddScoped<IMedicoRepository, JsonMedicoRepository>();
 builder.Services.AddScoped<ICitaRepository, JsonCitaRepository>();
 
-// Configuración de PostgreSQL 
+// Configuración de PostgreSQL
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 
-// Configurar Identity (Usuarios y Roles)
+// Configurar Identity (Usuarios y Roles) 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
-
-
 var app = builder.Build();
-
-
 
 if (!app.Environment.IsDevelopment())
 {
@@ -40,10 +37,15 @@ app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapStaticAssets();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
+
+
+app.MapRazorPages();
 
 app.Run();
